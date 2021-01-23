1c544b1090a8fca1070ef608ef99f5e3b9e1cc6a
Alexander Sworski <mail@dev-sworski.com>
2021-01-23 16:10:54 +0100

    Weather data from OpenWeatherMap API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
+import '../services/networking.dart';
 import 'package:clima/services/config.dart'; // API FIlE in .gitignore
 
 class LoadingScreen extends StatefulWidget {
@@ -11,32 +11,27 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   Location loc;
+  var WeatherJson;
 
-  Future<void> getLocation() async {
+  Future<void> getLocationData() async {
     loc = Location();
     loc.getCurrentLocation();
-  }
-
-  void getData() async {
-    await getLocation();
-    http.Response response = await http.get(
+    APIHelper api = APIHelper(
         "https://samples.openweathermap.org/data/2.5/weather?lat=${loc.getLat()}&lon=${loc.getLong()}&appid=${OPENWEATHERMAP_API_KEY}");
-    if (response.statusCode == 200)
-      print(response.body);
-    else
-      print(response.statusCode);
+    WeatherJson = await api.getData();
+    var city = WeatherJson['name'];
+    var temp = WeatherJson['main']['temp'];
+    var description = WeatherJson['weather'][0]['description'];
   }
 
   @override
   void initState() {
-    print("init");
-    getData();
+    getLocationData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build");
     return Scaffold(
       body: Center(child: Text(loc.toString())),
     );
