746fd8bf3bd83ef4c50528059426ccd95cf4d87e
Alexander Sworski <mail@dev-sworski.com>
2021-01-23 19:27:25 +0100

    Passing data through the nav stack

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -17,20 +17,9 @@ class WeatherModel {
 
   Future<dynamic> getCityWeather(String city_name) async {
     APIHelper api = APIHelper(
-        "https://samples.openweathermap.org/data/2.5/weather?q=${urify(city_name)}&appid=${OPENWEATHERMAP_API_KEY}");
+        "https://samples.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${OPENWEATHERMAP_API_KEY}");
     print(api.url);
     return await api.getData();
-  }
-
-  /**
-   * Makes sure that the String can be used in a URL.
-   * Methode is incomplete
-   * TODO: Add other conditions
-   */
-  String urify(String text) {
-    String RESULT = "";
-    RESULT = text.replaceAll(' ', '%20');
-    return RESULT;
   }
 
   String getWeatherIcon(int condition) {
